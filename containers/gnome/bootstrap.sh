1e584d2c726439c502a7f0a75c0cfccf4f22524a
Atte Silen <30522886+atzufuki@users.noreply.github.com>
2025-09-27 17:27:03 +0300

    Update gnome bootstrapper

--- containers/gnome/bootstrap.sh
+++ containers/gnome/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-sudo dnf group install workstation-product-environment -y
+if ! sudo dnf group list installed | grep -q "workstation-product-environment"; then
+    sudo dnf group install workstation-product-environment -y
+else
+    echo "workstation-product-environment group is already installed."
+fi
 
 echo "GNOME container bootstrap complete!"
