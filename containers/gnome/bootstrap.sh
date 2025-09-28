52b89e1a7a25c7a8ed23c4947e1bd7d1e1115632
Atte Silen <30522886+atzufuki@users.noreply.github.com>
2025-09-28 17:17:52 +0300

    Testing

--- containers/gnome/bootstrap.sh
+++ containers/gnome/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo dnf update -y
+
 if ! sudo dnf group list installed | grep -q "workstation-product-environment"; then
     sudo dnf group install workstation-product-environment -y
 else
