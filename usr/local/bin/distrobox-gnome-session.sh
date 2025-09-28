24dc939faa96271aec906a7effe3e876beb0ecfc
Atte Silen <30522886+atzufuki@users.noreply.github.com>
2025-09-28 15:21:50 +0300

    Testing

--- usr/local/bin/distrobox-gnome-session.sh
+++ usr/local/bin/distrobox-gnome-session.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-/usr/bin/distrobox-enter -n fedora-gnome --additional-flags "--env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR --env WAYLAND_DISPLAY=$WAYLAND_DISPLAY" -- /usr/bin/gnome-session > /home/atzufuki/gnome-session.log 2>&1
+/usr/bin/distrobox-enter -n fedora-gnome --additional-flags "--env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR --env WAYLAND_DISPLAY=$WAYLAND_DISPLAY" -- /usr/bin/gnome-session --debug
+journalctl --user -b | grep gnome-session > /home/atzufuki/gnome-session-journal.log
