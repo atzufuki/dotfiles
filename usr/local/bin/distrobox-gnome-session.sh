a6af7ee3f676de8458728b70639810025c3c4a31
Atte Silen <30522886+atzufuki@users.noreply.github.com>
2025-09-28 19:00:34 +0300

    Testing

--- usr/local/bin/distrobox-gnome-session.sh
+++ usr/local/bin/distrobox-gnome-session.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+xhost +si:localuser:$USER
 # Only Wayland mounts and flags
 /usr/bin/distrobox-enter -T -n fedora-gnome --additional-flags "--env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR --env WAYLAND_DISPLAY=$WAYLAND_DISPLAY" -- /usr/bin/gnome-session
