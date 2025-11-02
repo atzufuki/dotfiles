c3335ab3a22f0026c97374c59c6d79b455bc3171
Atte Silen <30522886+atzufuki@users.noreply.github.com>
2025-11-02 21:50:43 +0200

    Testing

--- host/bin/weston-gnome-launcher.sh
+++ host/bin/weston-gnome-launcher.sh
@@ -10,9 +10,9 @@ if [ -n "$WAYLAND_DISPLAY" ]; then
     BACKEND="wayland-backend.so"
     echo "Detected existing Wayland session, using wayland backend"
 else
-    # Running on TTY/GDM, use DRM
+    # Running on TTY/GDM, use DRM with software rendering
     BACKEND="drm-backend.so"
-    echo "Running on TTY, using DRM backend"
+    echo "Running on TTY, using DRM backend with software rendering"
 fi
 
 # Force use of logind by setting LIBSEAT_BACKEND
@@ -21,6 +21,10 @@ export LIBSEAT_BACKEND=logind
 # Use logind launcher for seat management instead of seatd
 export XDG_SEAT=seat0
 export XDG_SESSION_TYPE=wayland
+
+# Force software rendering (pixman) for VMware compatibility
+# This avoids EGL/GL issues in virtual machines
+export WESTON_RENDERER=pixman
 
 # Launch weston in fullscreen mode with the GNOME session launcher
 exec weston --backend=$BACKEND --shell=fullscreen-shell.so -- /usr/local/bin/gnome-session.sh
